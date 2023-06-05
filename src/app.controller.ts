1568800d2a823da16e4023e1981140606ad3e22e
valentinesamuel <valentinesamuel2580@gmail.com>
2023-06-05 16:53:50 +0100

    FEAT: Edited controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,8 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
   async getHello(): Promise<any> {
-    await this.appService.seed();
-    // await this.appService.deleteEmployee(1);
-    await this.appService.deleteContactInfo(1);
-    return;
+    return 'Hiii';
   }
 }
